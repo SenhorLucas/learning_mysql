a939bd822bcace3f642c2918f2a66d548afc8f89
Lucas Viana <lucas.nviana@gmail.com>
2018-09-22 15:19:27 +0000

    created some fun queries to investigate the dummy instagram database

--- insta_queries.sql
+++ insta_queries.sql
@@ -2,10 +2,16 @@
 SELECT 
     users.username, 
     users.created_at, 
-    COUNT(photos.id) AS nphotos
+    COUNT(photos.id) AS nphotos,
     MAX(photos.created_at) AS latest_post
 FROM users
 JOIN photos
     ON users.id = photos.user_id
 GROUP BY users.id
 ORDER BY users.created_at DESC LIMIT 5;
+
+-- Figure out which day of the week most users regist on
+SELECT
+    DAYOFWEEK(created_at) AS day_of_week
+FROM users
+ORDER BY created_at;
